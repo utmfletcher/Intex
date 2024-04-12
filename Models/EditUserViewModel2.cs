5dfa423805f03830580e56e6bfe555a94290e6af
Chad Sauder <chadsauder22@gmail.com>
2024-04-11 23:29:13 -0600

    no

---
+++ Models/EditUserViewModel2.cs
@@ -0,0 +1,12 @@
+﻿namespace Intex.Models
+{
+    public class EditUserViewModel2
+    {
+    }
+}
+public class EditUserViewModel
+{
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+}
